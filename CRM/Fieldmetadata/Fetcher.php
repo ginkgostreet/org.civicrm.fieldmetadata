2e2912457c2b0d1c8d7d0c7be353af9ea5e4892c
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-08-29 13:13:17 -0700

    FIS-47: Added 'getInstanceForEntity instantiation function to registers Fetcher classes via the registerFetcher hook, and returns a sub-class instance for a given entity

--- CRM/Fieldmetadata/Fetcher.php
+++ CRM/Fieldmetadata/Fetcher.php
@@ -9,6 +9,34 @@
 
 class CRM_Fieldmetadata_Fetcher {
 
-  
+  /**
+   * Instantiation function to get an instance of a Fetcher
+   * sub-class for a given entity
+   *
+   * @param $entity
+   * @return CRM_Case_Form_CustomData
+   * @throws CRM_Core_Exception
+   */
+  public static function &getInstanceForEntity($entity) {
+    // key: Entity => value: PHP class
+    $fetcherClasses = array();
+    CRM_Fieldmetadata_Hook::registerFetcher($fetcherClasses);
+    $class = CRM_Utils_Array::value($entity, $fetcherClasses);
 
+    if (!$class) {
+      // throw exception indicating no fetcher
+      // has been registered for this entity
+      throw new CRM_Core_Exception("No Fetcher class has been registered for '{$entity}'", 1);
+    }
+
+    $fetcher = new $class;
+
+    if (!is_subclass_of($fetcher, "CRM_Fieldmetadata_Fetcher")) {
+      // throw exception indicating the provided class
+      // does not extend the required base class
+      throw new CRM_Core_Exception("Fetcher class '{$class}' does not extend the 'CRM_Fieldmetadata_Fetcher' base class", 2);
+    }
+
+    return $fetcher;
+  }
 }
