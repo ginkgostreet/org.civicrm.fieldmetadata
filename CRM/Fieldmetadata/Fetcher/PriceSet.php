3ff2eb0895ab650316e6935fd0e67b5a9e2160a4
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-08-30 07:34:55 -0700

    FIS-49: Fixed Copy-Paste error

--- CRM/Fieldmetadata/Fetcher/PriceSet.php
+++ CRM/Fieldmetadata/Fetcher/PriceSet.php
@@ -13,16 +13,17 @@ class CRM_Fieldmetadata_Fetcher_PriceSet extends CRM_Fieldmetadata_Fetcher {
     $id = CRM_Utils_Array::value("id", $params);
 
     $result = civicrm_api3("PriceSet", "get", array(
+      'sequential' => 1,
       'id' => $id,
       'api.PriceField.get' => array(
         'options' => array('limit' => 0),
-        'api.PriceField.get' => array(
+        'api.PriceFieldValue.get' => array(
           'options' => array('limit' => 0),
         ),
       ),
     ));
 
-    return $result['values'];
+    return $result['values'][0];
   }
 
 }
