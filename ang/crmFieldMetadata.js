32cf0f9688aa32f1431b80cc666d0a09ce4bc6dd
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-09-22 11:09:09 -0700

    FIS-51: Added crmFieldMetadataTotal service that creates a watcher and triggers an updated total fee

--- ang/crmFieldMetadata.js
+++ ang/crmFieldMetadata.js
@@ -17,4 +17,46 @@
     return crmFieldMetadataFetch;
   });
 
+
+  angular.module('crmFieldMetadata').factory('crmFieldMetadataTotal', function(){
+    var crmFieldMetadataTotal = function($scope, metadata, data, result) {
+      var priceFields = {};
+
+      $.each(metadata.values.fields, function(name, field) {
+        if (field.quantity == 1 && field.price) {
+          priceFields[name] = parseFloat(field.price);
+        } else if (field.options) {
+          $.each(field.options, function(index, option) {
+            if (option.price) {
+              priceFields[name + "_" + option.value] = parseFloat(option.price);
+            }
+          });
+        }
+      });
+
+      $scope.$watch(data, function (newValue, oldValue, scope) {
+        var total = 0.00;
+
+        $.each(newValue, function(name, value) {
+          if (metadata.values.fields[name].quantity && priceFields.hasOwnProperty(name)) {
+            total = total + (parseInt(value) * priceFields[name]);
+          } else {
+            if(typeof value === "object") {
+              $.each(value, function(optionId, checked) {
+                if (checked && priceFields.hasOwnProperty(name + "_" + optionId)) {
+                  total = total + priceFields[name + "_" + optionId];
+                }
+              });
+            } else if (priceFields.hasOwnProperty(name + "_" + value)) {
+              total = total + priceFields[name + "_" + value];
+            }
+          }
+        });
+
+        $scope[result] = total;
+      }, true);
+    };
+    return crmFieldMetadataTotal;
+  });
+
 })(angular, CRM.$, CRM._);
