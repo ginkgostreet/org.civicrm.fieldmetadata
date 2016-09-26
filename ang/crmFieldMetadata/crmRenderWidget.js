bfb4848d4ee1f6b8160d3d34215a547de208713d
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-09-26 11:24:58 -0700

    FIS-34: Fleshed out the Render Widget directive to take more than price fields into account

--- ang/crmFieldMetadata/crmRenderWidget.js
+++ ang/crmFieldMetadata/crmRenderWidget.js
@@ -11,9 +11,22 @@
       link: function(scope, elem, attrs) {
         //console.log(scope.field.widget);
         scope.field.displayPrice = (scope.field.displayPrice == 1);
+        var childEl;
         switch (scope.field.widget) {
+          case "crm-ui-datepicker":
+            var tmp = $compile('<input crm-ui-datepicker="{time: false}" ng-model="model" ng-required="field.required" />')(scope);
+            childEl = tmp.parent();
+            break;
+          case "crm-entityref":
+            childEl = $compile('<span class="crm-entitiyref-wrapper"><input crm-entityref="{entity: \'' + scope.field.entity + '\', select: {allowClear:!field.required}}" field="field" ng-model="model" prefix="prefix" ng-required="field.required" /></span>')(scope);
+            console.log(childEl);
+            break;
           default:
-            var childEl = $compile('<div ' + scope.field.widget + ' field="field" model="model" prefix="prefix"></div>')(scope);
+            if (scope.field.widget.indexOf('crm-render') === -1) {
+              childEl = $compile('<input ' + scope.field.widget + ' field="field" ng-model="model" prefix="prefix" ng-required="field.required" />')(scope);
+            } else {
+              childEl = $compile('<div ' + scope.field.widget + ' field="field" model="model" prefix="prefix" ng-required="field.required"></div>')(scope);
+            }
         }
 
         elem.addClass("crmRenderWidget");
