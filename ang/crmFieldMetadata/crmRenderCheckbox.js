0bd8ab4f59f5787199814be72033fc5c09364d44
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-10-04 02:11:46 -0700

    Updated crmRenderCheckbox to properly handle undefined models and with defaults

--- ang/crmFieldMetadata/crmRenderCheckbox.js
+++ ang/crmFieldMetadata/crmRenderCheckbox.js
@@ -10,6 +10,16 @@
       templateUrl: '~/crmFieldMetadata/crmRenderCheckbox.html',
       controller: ['$scope', function crmRenderCheckboxController($scope) {
         $scope.formatMoney = CRM.formatMoney;
+        //if the model is undefined (eg an undefined key on an object
+        //AND we have multiple options, set the base model to an object
+        //so that the bindings work.
+        if (typeof($scope.model) === "undefined" && $scope.field.options.length > 0) {
+          $scope.model = {};
+          _.each($scope.field.options, function(option) {
+            $scope.model[option.value] = !!option.default;
+          });
+
+        }
       }]
     };
   });
