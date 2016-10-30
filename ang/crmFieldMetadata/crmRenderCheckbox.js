ecfa6421c80e3bbcfeec19ccdc00b8e4b23754cb
Frank J. Gómez <frank@ginkgostreet.com>
2016-10-30 14:54:11 -0400

    Refactored crmRenderCheckbox directive.
    Improvements:
    - Boxes are now checked as appropriate after records are retrieved from the server.
    - Better handling of types when server reports no boxes are checked.
    - Model reflects all the checked boxes in a checklist group (rather than having one model per checkbox),
      consistent with CiviCRM's API.
    - Checkboxes are marked required based on the field metadata rather than the option metadata -- is
      there such a thing as a required option? -- taking into account whether other boxes are checked.
    
    Works in progress:
    - Form validation doesn't work, despite a proper result in ng-required. To fix this I think there are
      two possible approaches: define ngModel on the element using a getterSetter, or override $isEmpty for
      the directive.

--- ang/crmFieldMetadata/crmRenderCheckbox.js
+++ ang/crmFieldMetadata/crmRenderCheckbox.js
@@ -10,15 +10,27 @@
       templateUrl: '~/crmFieldMetadata/crmRenderCheckbox.html',
       controller: ['$scope', function crmRenderCheckboxController($scope) {
         $scope.formatMoney = CRM.formatMoney;
-        //if the model is undefined (eg an undefined key on an object
-        //AND we have multiple options, set the base model to an object
-        //so that the bindings work.
-        if (typeof($scope.model) === "undefined" && $scope.field.options.length > 0) {
-          $scope.model = {};
-          _.each($scope.field.options, function(option) {
-            $scope.model[option.value] = !!option.default;
-          });
+        $scope.handleToggle = function (value) {
+          var i = $scope.model.indexOf(value);
+          if (i === -1) {
+            $scope.model.push(value);
+          } else {
+            $scope.model.splice(i, 1);
+          }
+        };
+        $scope.isChecked = function (value) {
+          return ($scope.model.indexOf(value) === -1 ? false : true);
+        };
+        $scope.isRequired = function () {
+          return $scope.field.required && ($scope.model.length === 0);
+        };
 
+        // Checkboxes are modeled as Arrays because multiple selections can be
+        // made. However, if we are retrieving a set of checkboxes for which no
+        // selections have been made, CiviCRM's API represents the field as an
+        // empty string. We standardize the representation here.
+        if (!Array.isArray($scope.model)) {
+          $scope.model = [];
         }
       }]
     };
