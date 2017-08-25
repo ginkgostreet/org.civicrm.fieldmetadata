8c772b6d8010313e580754c9f77abc074980a38b
Frank J. Gómez <frank@ginkgostreet.com>
2017-08-25 12:29:10 -0400

    Fixes #15: Adds support for custom fields of type boolean.

--- CRM/Fieldmetadata/Normalizer/CustomGroup.php
+++ CRM/Fieldmetadata/Normalizer/CustomGroup.php
@@ -38,26 +38,16 @@ class CRM_Fieldmetadata_Normalizer_CustomGroup extends CRM_Fieldmetadata_Normali
 
         // This check is made because API chaining is not reliable in cases where
         // the value that is chained off of is NULL. See CRM-17327, for example.
+        // TODO: Possibly the better fix for this is not to chain at all and to
+        // use the API's join capabilities instead.
         if (!empty($customField['option_group_id'])) {
-          $fieldOptions = CRM_Utils_Array::value("values", $customField['api.OptionValue.get'], array());
-          foreach ($fieldOptions as $fieldOption) {
-            if ($fieldOption['is_active'] == 1) {
-              $option = $this->getEmptyOption();
-              $option['is_active'] = $this->normalizeBoolean($fieldOption['is_active']);
-              $option['label'] = $fieldOption['label'];
-              $option['order'] = $fieldOption['weight'];
-              $option['default'] = $this->normalizeBoolean($fieldOption['is_default']);
-              $option['value'] = $fieldOption['value'];
-
-              $option['name'] = $fieldOption['name'];
-              if ($customField['html_type'] == "CheckBox") {
-                $option['name'] = 'custom_' . $customField['id'] . '[' . $fieldOption['value'] . ']';
-              }
-
-              $field['options'][] = $option;
-            }
-          }
+          $field['options'] = $this->normalizeFieldOptions($customField);
         }
+
+        if ($customField['data_type'] === 'Boolean') {
+          $field['options'] = $this->mockBooleanOptions($fields);
+        }
+
         $fields[$field['name']] = $field;
       }
     }
@@ -65,4 +55,73 @@ class CRM_Fieldmetadata_Normalizer_CustomGroup extends CRM_Fieldmetadata_Normali
     $return['fields'] = $fields;
     return $return;
   }
+
+  /**
+   * @param array $customField
+   *   Looks like the result of api.CustomField.getsingle.
+   * @return array
+   */
+  private function normalizeFieldOptions(array $customField) {
+    $result = array();
+    $fieldOptions = CRM_Utils_Array::value("values", $customField['api.OptionValue.get'], array());
+    foreach ($fieldOptions as $fieldOption) {
+      if ($fieldOption['is_active'] == 1) {
+        $option = $this->getEmptyOption();
+        $option['is_active'] = $this->normalizeBoolean($fieldOption['is_active']);
+        $option['label'] = $fieldOption['label'];
+        $option['order'] = $fieldOption['weight'];
+        $option['default'] = $this->normalizeBoolean($fieldOption['is_default']);
+        $option['value'] = $fieldOption['value'];
+
+        $option['name'] = $fieldOption['name'];
+        if ($customField['html_type'] == "CheckBox") {
+          $option['name'] = 'custom_' . $customField['id'] . '[' . $fieldOption['value'] . ']';
+        }
+
+        $result[] = $option;
+      }
+    }
+
+    return $result;
+  }
+
+  /**
+   * Boolean fields in CiviCRM do not use an option group, so we have to
+   * simulate the options.
+   *
+   * @param array $customField
+   *   Looks like the result of api.CustomField.getsingle.
+   * @return array
+   */
+  private function mockBooleanOptions(array $customField) {
+    $result = array();
+    $fieldOptions = array(
+      array(
+        'label' => 'Yes',
+        'weight' => 0,
+        'value' => "1",
+      ),
+      array(
+        'label' => 'No',
+        'weight' => 1,
+        'value' => "0",
+      ),
+    );
+    foreach ($fieldOptions as $fieldOption) {
+      $option = $this->getEmptyOption();
+      $option['is_active'] = $this->normalizeBoolean(1);
+      $option['label'] = $fieldOption['label'];
+      $option['order'] = $fieldOption['weight'];
+      $option['value'] = $fieldOption['value'];
+
+      $isDefault = $customField['default_value'] === $option['value'];
+      $option['default'] = $this->normalizeBoolean($isDefault);
+      $option['name'] = 'custom_' . $customField['id'] . '[' . $fieldOption['value'] . ']';
+
+      $result[] = $option;
+    }
+
+    return $result;
+  }
+
 }
