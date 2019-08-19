75d4c3bb8cf1dfc193f09f91cdd53cc5f78e04c0
ginkgoMZD <mzd@ginkgostreet.com>
2019-08-19 16:21:54 -0400

    fix indeterminate results from api.OptionValue.get; add sort by weight;

--- CRM/Fieldmetadata/Fetcher/CustomGroup.php
+++ CRM/Fieldmetadata/Fetcher/CustomGroup.php
@@ -14,6 +14,8 @@ class CRM_Fieldmetadata_Fetcher_CustomGroup extends CRM_Fieldmetadata_Fetcher {
       'api.OptionValue.get' => array(
         'limit' => 0,
         'option_group_id' => '$value.option_group_id',
+        'options' => array('sort' => "weight"),
+        'is_active' => 1
       )
     );
 
